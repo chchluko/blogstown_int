312877888d87ef618b630caa1808a667fe53ebdc
Jesus Lopez Eulogio <A15774@GPROA.AH>
2020-11-20 14:28:23 -0600

    se quita excel del desarrollo

--- config/app.php
+++ config/app.php
@@ -177,7 +177,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Spatie\Permission\PermissionServiceProvider::class,
-        Maatwebsite\Excel\ExcelServiceProvider::class,
+        //Maatwebsite\Excel\ExcelServiceProvider::class,
 
     ],
 
@@ -231,7 +231,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'JWTAuth' => Tymon\JWTAuth\Facades\JWTAuth::class,
 'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class,
-'Excel' => Maatwebsite\Excel\Facades\Excel::class,
+//'Excel' => Maatwebsite\Excel\Facades\Excel::class,
 
     ],
 
